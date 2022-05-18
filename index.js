eba980402a4be5c4b10d15dfea195fdf402464ae
uhveg <ulises.1124@gmail.com>
2022-05-18 15:09:52 -0500

    Ulises commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var item = document.getElementById("contain");
 
-fetch("https://github.com/uhveg/Website_infocards/blob/master/static/list_jobs.json")
+fetch("https://raw.githubusercontent.com/uhveg/Website_infocards/master/static/list_jobs.json")
 .then(response => {
     return response.json();
 })
